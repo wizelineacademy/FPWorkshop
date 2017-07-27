795af7c316c8400c48a63acc71f5d0afb7e886fa
Charlie <charlie@wizeline.com>
2017-07-27 16:55:21 -0500

    instructions for users problems

--- samples/users.js
+++ samples/users.js
@@ -16,9 +16,17 @@ const getWebsitesWithOrg = (users) => {
 /*
  * Given an unordered list of objects that represent users, write a function "getFinalAddresses" that receives those users
  *
- * The function should return the websites from the users that contains the domain 'org' from the list
+ * The function should return all the props from the address and create a single string with all of them.
  *
- * getWebsitesWithOrg(users) -> List<Websites>
+ * address -
+ *  - street
+ *  - suite
+ *  - city
+ *  - zipcode
+ *
+ *  result => street + suite + city + zipcode
+ *
+ *getFinalAddresses(users) -> String
  *
  * */
 
